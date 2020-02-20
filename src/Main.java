6f51204edbe002366e2f1c12c76c78522ae37bcb
rezjava <rez.neox@gmail.com>
2020-02-20 15:15:30 +0200

    class Menu has been added

--- src/Main.java
+++ src/Main.java
@@ -9,54 +9,26 @@ public class Main {
     private static final String FILE2 = "/home/IdeaProjects/ActiveVocabulary/list2";
     private static final String FILE3 = "/home/IdeaProjects/ActiveVocabulary/list3";
 
-    private static void showMenu() {
-        System.out.println("|===========================|");
-        System.out.println("|     MENU SELECTION        |");
-        System.out.println("|===========================|");
-        System.out.println("|     Options:              |");
-        System.out.println("|  1. reading of the list   |");
-        System.out.println("|  2. spelling of the words |");
-        System.out.println("| 'q' exit from program     |");
-        System.out.println("|===========================|");
-        System.out.println("*****************************");
-    }
-
     private static boolean isChoiceValid(int ch) {
         if (ch < '1' | ch > '3' & ch != 'q') return false;
         return true;
     }
 
-
     public static void main(String[] args) throws IOException, NoSuchFieldException {
-
-        char choice, ignore;
         label:
         {
             Word obj = new Word();
             ReadingOfList readingOfList = new ReadingOfList();
             Spelling spelling = new Spelling();
-
+            Menu menu = new Menu();
             obj.readMainFile(MAINFILE, FILE1);
             readingOfList.readFile1(FILE1);
+            char choice;
             do {
-                showMenu();
-                System.out.print("input your choice: ");
-                choice = (char) System.in.read();
-                do {
-                    ignore = (char) System.in.read();
-                } while (ignore != '\n');
-
-                switch (choice) {
-                    case '1':
-                        System.out.println("reading of the list all words");
-                        break;
-                    case '2':
-                        System.out.println("spelling of the words");
-                        break;
-                    case 'q':
-                        System.out.println("quiet");
-                        break label;
-                }
+                menu.showMenu();
+                choice = menu.takeChoise();
+                if (choice == 'q')
+                    break label;
             } while (!isChoiceValid(choice));
         }
     }
---
+++ src/Menu.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+
+public class Menu {
+
+    public void showMenu() {
+        System.out.println("|===========================|");
+        System.out.println("|     MENU SELECTION        |");
+        System.out.println("|===========================|");
+        System.out.println("|     Options:              |");
+        System.out.println("|  1. reading of the list   |");
+        System.out.println("|  2. spelling of the words |");
+        System.out.println("| 'q' exit from program     |");
+        System.out.println("|===========================|");
+        System.out.println("*****************************");
+    }
+    public char takeChoise() throws IOException {
+        char choice, ignore;
+        System.out.print("input your choice: ");
+        choice = (char) System.in.read();
+        do {
+            ignore = (char) System.in.read();
+        } while (ignore != '\n');
+
+        switch (choice) {
+            case '1':
+                System.out.println("reading of the list all words");
+                break;
+            case '2':
+                System.out.println("spelling of the words");
+                break;
+            case 'q':
+                System.out.println("quiet");
+                choice = 'q';
+                break;
+        }
+        return choice;
+    }
+}
