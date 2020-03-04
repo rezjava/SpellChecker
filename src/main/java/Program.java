2a7c4511444e17c3553775f1a50676569da4b473
rezjava <rez.neox@gmail.com>
2020-03-04 10:39:31 +0200

    have been added checking for file existence

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -2,15 +2,20 @@ import java.io.File;
 import java.io.IOException;
 
 public class Program {
-    private static final String MAIN_FILE = "/home/ubuntu/IdeaProjects/ActiveVocabulary/mainfile.txt.txt";
-    private static final String WORK_FILE = "/home/ubuntu/IdeaProjects/ActiveVocabulary/workfile.txt";
+    private static final String WORK_FILE = "/home/workfile.txt";
 
     public static void main(String[] args) throws IOException, NoSuchFieldException {
-
-        if (isFileExists(args[0]))
-            System.out.println("You have to input ");
+        final String mainFile = args[0];
         Program mainObject = new Program();
-        mainObject.run();
+        if (isFileExists(args[0])) {
+            if (isFileExists(WORK_FILE)) {
+                mainObject.run(mainFile);
+            } else {
+                File file = new File("/home/workfile.txt");
+                mainObject.run(mainFile);
+            }
+        } else
+            System.out.println("You have to create your Vocabulary file and input it as parameter of program");
     }
 
     private boolean isChoiceValid(int ch) {
@@ -21,12 +26,12 @@ public class Program {
     private static boolean isFileExists(String filename) {
         File chack;
         try {
-            chack = File.createTempFile("/home/ubuntu/IdeaProjects/ActiveVocabulary/mainfile.txt", ".txt");
+            chack = File.createTempFile(filename, ".txt");
 
             boolean exists = chack.exists();
             if (exists) return true;
             else {
-                System.out.println("Vocabulary file doesn't exists");
+                System.out.println("file doesn't exists");
             }
 
         } catch (IOException e) {
@@ -35,15 +40,14 @@ public class Program {
         return false;
     }
 
-    private void run() throws IOException {
-
+    private void run(String mainFile) throws IOException {
 
         Word obj = new Word();
         ReadingOfFile readingOfList = new ReadingOfFile();
         Spelling spelling = new Spelling();
         Menu menu = new Menu();
         ReadingOfFile reading = new ReadingOfFile();
-        reading.readMainFile(MAIN_FILE, WORK_FILE);
+        reading.readMainFile(mainFile, WORK_FILE);
         reading.readFile1(WORK_FILE);
         char choice;
         do {
